cc2083d5ac4d77640d7cc7ca122fe1bd1c757802
Zaliye Prasandike <73892701+Saliya99@users.noreply.github.com>
2023-03-15 00:54:17 +0530

    splash screen

--- rock_vin_cafe_app/lib/pages/splash/splash_page.dart
+++ rock_vin_cafe_app/lib/pages/splash/splash_page.dart
@@ -1,5 +1,8 @@
+import 'dart:async';
 import 'dart:ffi';
 import 'package:flutter/material.dart';
+import 'package:flutter/src/widgets/framework.dart';
+import 'package:get/get.dart';
 
 class SplashScreen extends StatefulWidget {
   const SplashScreen({super.key});
@@ -13,17 +16,31 @@ class _SplashScreenState extends State<SplashScreen>
   late Animation<double> animation;
   late AnimationController controller;
 
-  // controller =
-  //     AnimationController(vsync: this, duration: const Duration(seconds: 2));
-  // animation = CurvedAnimation(parent: controller, curve: Curves.linear);
+  @override
+  Void get initState {
+    super.initState;
+    controller =
+        AnimationController(vsync: this, duration: const Duration(seconds: 2))
+          ..forward();
+    animation = CurvedAnimation(parent: controller, curve: Curves.linear);
+
+    Timer(const Duration(seconds: 3),
+        () => Get.offNamed(RouteHelper.getinitial()));
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Column(children: [
-        Image.asset(
-          "assets/image/logopart1.png",
-          width: 250,
+      backgroundColor: Colors.white,
+      body: Column(mainAxisAlignment: MainAxisAlignment.center, children: [
+        ScaleTransition(
+          scale: animation,
+          child: Center(
+            child: Image.asset(
+              "assets/image/logopart1.png",
+              width: 250,
+            ),
+          ),
         )
       ]),
     );
