b949e87beefec085f6423b42f77ad2c6697a2f85
Gagana Chiranjitha <gaganachiranjithacoc2@gmail.com>
2023-03-15 08:27:40 +0530

    Revert "slider_update"
    This reverts commit 6470a72beb30ba787de032d737080c52afab378e.

--- rock_vin_cafe_app/lib/home/main_food_page.dart
+++ rock_vin_cafe_app/lib/home/main_food_page.dart
@@ -1,7 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/src/widgets/framework.dart';
 import 'package:rock_vin_cafe_app/colors.dart';
-import 'package:rock_vin_cafe_app/home/food_page_body.dart';
 import 'package:rock_vin_cafe_app/widgets/big_text.dart';
 import 'package:rock_vin_cafe_app/widgets/small_text.dart';
 
@@ -21,42 +20,40 @@ class _MainFoodPage extends State<MainFoodPage> {
       body: Column(
         children: [
           Container(
-            child: Container(
-              margin: EdgeInsets.only(top: 45, bottom: 15),
-              padding: EdgeInsets.only(left: 20, right: 20),
-              child: Row(
-                mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                children: [
-                  Column(
-                    children: [
-                      BigText(text: "Sri Lanka", color: AppColors.mainColor),
-                      Row(
-                        children: [
-                          SmallText(
-                            text: "Midigaama",
-                            color: Colors.black54,
-                          ),
-                          Icon(Icons.arrow_drop_down)
-                        ],
-                      )
-                    ],
-                  ),
-                  Center(
-                    child: Container(
-                      width: 45,
-                      height: 45,
-                      child: Icon(Icons.search, color: Colors.white),
-                      decoration: BoxDecoration(
-                        borderRadius: BorderRadius.circular(15),
-                        color: AppColors.mainColor,
-                      ),
+              child: Container(
+            margin: EdgeInsets.only(top: 45, bottom: 15),
+            padding: EdgeInsets.only(left: 20, right: 20),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                Column(
+                  children: [
+                    BigText(text: "Sri Lanka", color: AppColors.mainColor),
+                    Row(
+                      children: [
+                        SmallText(
+                          text: "Midigama",
+                          color: Colors.black54,
+                        ),
+                        Icon(Icons.arrow_drop_down)
+                      ],
+                    )
+                  ],
+                ),
+                Center(
+                  child: Container(
+                    width: 45,
+                    height: 45,
+                    child: Icon(Icons.search, color: Colors.white),
+                    decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(15),
+                      color: AppColors.mainColor,
                     ),
-                  )
-                ],
-              ),
+                  ),
+                )
+              ],
             ),
-          ),
-          FoodPageBody(),
+          ))
         ],
       ),
     );
