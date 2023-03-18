7a81352dc1a702a7b41268cd78d70740e8e07bac
Zaliye Prasandike <73892701+Saliya99@users.noreply.github.com>
2023-03-18 15:26:41 +0530

    recommended_food_detail SliverAppBar, SliverToBoxAdapter

--- rock_vin_cafe_app/lib/pages/food/recommended_food_detail.dart
+++ rock_vin_cafe_app/lib/pages/food/recommended_food_detail.dart
@@ -1,6 +1,13 @@
+import 'dart:html';
+
 import 'package:flutter/material.dart';
 import 'package:flutter/src/widgets/container.dart';
 import 'package:flutter/src/widgets/framework.dart';
+import 'package:rock_vin_cafe_app/utils/colors.dart';
+import 'package:rock_vin_cafe_app/utils/dimensions.dart';
+import 'package:rock_vin_cafe_app/widgets/app_icon.dart';
+import 'package:rock_vin_cafe_app/widgets/big_text.dart';
+import 'package:rock_vin_cafe_app/widgets/expandable_text_widget.dart';
 
 class RecommendedFoodDetails extends StatelessWidget {
   const RecommendedFoodDetails({super.key});
@@ -8,14 +15,58 @@ class RecommendedFoodDetails extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      backgroundColor: Colors.white,
       body: CustomScrollView(
         slivers: [
           SliverAppBar(
+            toolbarHeight: 70,
+            title: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  AppIcon(icon: Icons.clear),
+                  AppIcon(icon: Icons.shopping_cart_outlined)
+                ]),
+            bottom: PreferredSize(
+              preferredSize: const Size.fromHeight(20),
+              child: Container(
+                color: Colors.white,
+                child: Center(
+                    child: BigText(
+                  size: Dimensions.font26,
+                  text: "Cooked SEAFOOD Soup",
+                )),
+                width: double.maxFinite,
+                padding: EdgeInsets.only(top: 5, bottom: 10),
+                decoration: BoxDecoration(
+                    color: Colors.white,
+                    borderRadius: BorderRadius.only(
+                      topLeft: Radius.circular(Dimensions.radius20),
+                      topRight: Radius.circular(Dimensions.radius20),
+                    )),
+              ),
+            ),
+            pinned: true,
+            backgroundColor: AppColors.yellowColor,
+            expandedHeight: 300,
             flexibleSpace: FlexibleSpaceBar(
               background: Image.asset(
                 "assets/image/food0.png",
                 width: double.maxFinite,
+                fit: BoxFit.cover,
               ),
+            ),
+          ),
+          SliverToBoxAdapter(
+            child: Column(
+              children: [
+                Container(
+                  child: ExpandableTextWidget(
+                      text:
+                          "English Breakfast is a classic and hearty meal that is perfect for any time of the day, but especially popular for breakfast. This dish typically includes a combination of eggs, bacon, sausage, grilled tomato, mushrooms, and baked beans. It is served with toasted bread or fried bread, which is perfect for dipping into the delicious runny yolks of the eggs. The English Breakfast is a staple of British cuisine, and is loved by people all over the world. At our restaurant, we serve up a traditional English Breakfast that is made with high-quality ingredients, and is sure to satisfy your cravings. So, whether you're looking for a hearty breakfast, or just in the mood for some delicious comfort food, be sure to try our English Breakfast today!"),
+                  margin: EdgeInsets.only(
+                      left: Dimensions.width20, right: Dimensions.width20),
+                )
+              ],
             ),
           )
         ],
