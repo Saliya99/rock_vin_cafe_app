c237301a103b4c3c2b8a105601d6a92047c7e68e
Zaliye Prasandike <73892701+Saliya99@users.noreply.github.com>
2023-03-19 11:27:49 +0530

    Update app_icon.dart

--- rock_vin_cafe_app/lib/widgets/app_icon.dart
+++ rock_vin_cafe_app/lib/widgets/app_icon.dart
@@ -1,20 +1,22 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/src/widgets/framework.dart';
 import 'package:flutter/src/widgets/placeholder.dart';
+import 'package:rock_vin_cafe_app/utils/dimensions.dart';
 
 class AppIcon extends StatelessWidget {
   final IconData icon;
   final Color backgroundColor;
   final Color iconColor;
   final double size;
+  final double iconSize;
 
-  AppIcon({
-    super.key,
+  AppIcon({Key? key,
     required this.icon,
     this.backgroundColor = const Color(0xFFfcf4e4),
     this.iconColor = const Color(0xFF000000),
     this.size = 40,
-  });
+    this.iconSize = 16
+  }) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
@@ -28,7 +30,7 @@ class AppIcon extends StatelessWidget {
       child: Icon(
         icon,
         color: iconColor,
-        size: 16,
+        size: iconSize
       ),
     );
   }
