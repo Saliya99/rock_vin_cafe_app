0e748992667a81f3febc6d69c4a7d5bc8ebcced8
Zaliye Prasandike <73892701+Saliya99@users.noreply.github.com>
2023-03-19 11:56:08 +0530

    recommended food details page done

--- rock_vin_cafe_app/lib/pages/food/recommended_food_detail.dart
+++ rock_vin_cafe_app/lib/pages/food/recommended_food_detail.dart
@@ -73,15 +73,78 @@ class RecommendedFoodDetails extends StatelessWidget {
       bottomNavigationBar: Column(
         mainAxisSize: MainAxisSize.min,
         children: [
-          Row(
-            mainAxisAlignment: MainAxisAlignment.spaceBetween,
-            children: [
-              AppIcon(iconColor: Colors.white,
-                  backgroundColor: AppColors.mainColor,
-                  icon: Icons.remove),
-              AppIcon(icon: Icons.add)
-            ],
-          )
+          Container(
+            padding: EdgeInsets.only(
+              left: Dimensions.width20*2.5,
+              right: Dimensions.width20*2.5,
+              top: Dimensions.height10,
+              bottom: Dimensions.height10,
+            ),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                AppIcon(
+                    iconSize:Dimensions.iconSize24 ,
+                    iconColor: Colors.white,
+                    backgroundColor: AppColors.mainColor,
+                    icon: Icons.remove),
+                BigText(text: "\$12.88 "+" X "+" 0 ", color: AppColors.mainBlackColor,size: Dimensions.font26,),
+
+                AppIcon(
+                    iconSize:Dimensions.iconSize24 ,
+                    iconColor: Colors.white,
+                    backgroundColor: AppColors.mainColor,
+                    icon: Icons.add),
+              ],
+            ),
+          ),
+          Container(
+            height: 120,
+            padding: EdgeInsets.only(
+                top: Dimensions.height30,
+                bottom: Dimensions.height30,
+                left: Dimensions.width20,
+                right: Dimensions.width20),
+            decoration: BoxDecoration(
+                color: AppColors.buttonBackgroungColor,
+                borderRadius: BorderRadius.only(
+                    topLeft: Radius.circular(Dimensions.radius20 * 2),
+                    topRight: Radius.circular(Dimensions.radius20 * 2))),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                Container(
+                  padding: EdgeInsets.only(
+                      top: Dimensions.height20,
+                      bottom: Dimensions.height20,
+                      left: Dimensions.width20,
+                      right: Dimensions.width20),
+                  decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(Dimensions.radius20),
+                    color: Colors.white,
+                  ),
+                  child: Icon(
+                    Icons.favorite,
+                    color: AppColors.mainColor,
+                  ),
+                ),
+                Container(
+                  padding: EdgeInsets.only(
+                      top: Dimensions.height20,
+                      bottom: Dimensions.height20,
+                      left: Dimensions.width20,
+                      right: Dimensions.width20),
+                  child: BigText(
+                    text: "Rs.800  |  Add to cart",
+                    color: Colors.white,
+                  ),
+                  decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(Dimensions.radius20),
+                      color: AppColors.mainColor),
+                )
+              ],
+            ),
+          ),
         ],
       ),
     );
