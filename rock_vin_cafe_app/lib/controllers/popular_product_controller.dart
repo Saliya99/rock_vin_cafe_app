7d064a2904d9a08e3e620417a87fd8cc18d95bed
Zaliye Prasandike <73892701+Saliya99@users.noreply.github.com>
2023-05-07 16:45:47 +0530

    Api started

---
+++ rock_vin_cafe_app/lib/controllers/popular_product_controller.dart
@@ -0,0 +1,7 @@
+import 'package:get/get.dart';
+import 'package:rock_vin_cafe_app/data/repository/popular_product_repo.dart';
+
+class PopularProductController extends GetxController{
+  final PopularProductRepo popularProductRepo;
+  
+}
---
+++ rock_vin_cafe_app/lib/helper/dependencies.dart
@@ -0,0 +1,4 @@
+Future<void>init()async {
+
+
+}
