209964c3418f4b9d8149ab3c9ad311e0b3395fd6
Zaliye Prasandike <73892701+Saliya99@users.noreply.github.com>
2023-05-08 15:14:25 +0530

    updated

--- rock_vin_cafe_app/lib/main.dart
+++ rock_vin_cafe_app/lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:rock_vin_cafe_app/pages/splash/splash_page.dart';
+import 'package:get/get.dart';
 
 void main() {
   runApp(const MyApp());
@@ -14,25 +15,10 @@ class MyApp extends StatelessWidget {
     return const MaterialApp(
       debugShowCheckedModeBanner: false,
       title: 'Flutter Demo',
-<<<<<<< Updated upstream
-      //home: SplashScreen(),
-      initialRoute: RouteHelper.getSplashPage(),
-      getPages: RouteHelper.routes,
-=======
       theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
         primarySwatch: Colors.blue,
       ),
-      home: const MainFoodPage(),
->>>>>>> Stashed changes
+      home: Recommen(),
     );
   }
 }
