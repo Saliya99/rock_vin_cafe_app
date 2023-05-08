4f55dd47a685bfc8c4090c4756174c1ba6d07b43
Zaliye Prasandike <73892701+Saliya99@users.noreply.github.com>
2023-05-08 15:13:55 +0530

    popular_product_controller

--- rock_vin_cafe_app/lib/controllers/popular_product_controller.dart
+++ rock_vin_cafe_app/lib/controllers/popular_product_controller.dart
@@ -3,5 +3,18 @@ import 'package:rock_vin_cafe_app/data/repository/popular_product_repo.dart';
 
 class PopularProductController extends GetxController{
   final PopularProductRepo popularProductRepo;
-  
+  PopularProductController({required this.popularProductRepo});
+  List<dynamic> _popularProductList=[];
+  List<dynamic> get popularProductList => _popularProductList;
+
+  Future<void> getPopularProductList()async{
+    Response response = await popularProductRepo.getPopularProductList();
+    if(response.statusCode==200){
+      _popularProductList=[];
+      //_popularProductList.addAll();
+      update();
+    }else{
+
+    }
+  }
 }
