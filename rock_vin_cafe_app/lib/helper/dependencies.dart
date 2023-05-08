6c7de4fbd7b422f33bd8a20ccce1e99d946894ca
Zaliye Prasandike <73892701+Saliya99@users.noreply.github.com>
2023-05-08 15:14:10 +0530

    api dependencies

--- rock_vin_cafe_app/lib/helper/dependencies.dart
+++ rock_vin_cafe_app/lib/helper/dependencies.dart
@@ -1,4 +1,17 @@
-Future<void>init()async {
+import 'package:get/get.dart';
+import 'package:rock_vin_cafe_app/controllers/popular_product_controller.dart';
+import 'package:rock_vin_cafe_app/data/repository/popular_product_repo.dart';
 
+import '../data/api/api_client.dart';
+
+Future<void>init()async {
+  //api client
+  Get.lazyPut(()=>ApiClient(appBaseUrl:"https://www.dbestech.com"));
+  
+  //epos
+  Get.lazyPut(() => PopularProductRepo(apiClient:Get.find()));
+
+  //controller
+  Get.lazyPut(() => PopularProductController(popularProductRepo:Get.find() ));
 
 }
