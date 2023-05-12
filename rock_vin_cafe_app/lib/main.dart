da267ba405e0d39bfc57c91b87a6a76072048705
Gagana Chiranjitha <gaganachiranjithacoc2@gmail.com>
2023-05-12 14:24:46 +0530

    Update main

--- rock_vin_cafe_app/lib/main.dart
+++ rock_vin_cafe_app/lib/main.dart
@@ -4,8 +4,11 @@ import 'package:get/get.dart';
 import 'package:rock_vin_cafe_app/pages/food/popular_food_detail.dart';
 import 'package:rock_vin_cafe_app/pages/food/recommended_food_detail.dart';
 import 'package:rock_vin_cafe_app/pages/home/main_food_page.dart';
+import 'helper/dependencies.dart' as dep;
 
-void main() {
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await dep.init();
   runApp(const MyApp());
 }
 
@@ -15,17 +18,13 @@ class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-
     return GetMaterialApp(
-
       debugShowCheckedModeBanner: false,
       title: 'Flutter Demo',
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-
       home: RecommendedFoodDetails(),
-
     );
   }
 }
