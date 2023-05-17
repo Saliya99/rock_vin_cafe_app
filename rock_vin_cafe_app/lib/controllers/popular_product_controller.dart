0466e7be841fa1797d6ed82516806bc76fc55d24
Gagana Chiranjitha <gaganachiranjithacoc2@gmail.com>
2023-05-17 12:49:59 +0530

    popular_product_controller and api

--- rock_vin_cafe_app/lib/controllers/popular_product_controller.dart
+++ rock_vin_cafe_app/lib/controllers/popular_product_controller.dart
@@ -1,20 +1,19 @@
 import 'package:get/get.dart';
 import 'package:rock_vin_cafe_app/data/repository/popular_product_repo.dart';
+import 'package:rock_vin_cafe_app/models/products_model.dart';
 
-class PopularProductController extends GetxController{
+class PopularProductController extends GetxController {
   final PopularProductRepo popularProductRepo;
   PopularProductController({required this.popularProductRepo});
-  List<dynamic> _popularProductList=[];
+  List<dynamic> _popularProductList = [];
   List<dynamic> get popularProductList => _popularProductList;
 
-  Future<void> getPopularProductList()async{
+  Future<void> getPopularProductList() async {
     Response response = await popularProductRepo.getPopularProductList();
-    if(response.statusCode==200){
-      _popularProductList=[];
-      //_popularProductList.addAll();
+    if (response.statusCode == 200) {
+      _popularProductList = [];
+      _popularProductList.addAll(Product.fromJson(response.body).products);
       update();
-    }else{
-
-    }
+    } else {}
   }
 }
