184edb63e5d24dd2dfdfe3a2300a6e8f869d5315
Gagana Chiranjitha <gaganachiranjithacoc2@gmail.com>
2023-05-23 21:24:14 +0530

    update

--- rock_vin_cafe_app/lib/controllers/popular_product_controller.dart
+++ rock_vin_cafe_app/lib/controllers/popular_product_controller.dart
@@ -5,16 +5,21 @@ import 'package:rock_vin_cafe_app/models/products_model.dart';
 class PopularProductController extends GetxController {
   final PopularProductRepo popularProductRepo;
   PopularProductController({required this.popularProductRepo});
-  List<dynamic> _popularProductList = [];
-  List<dynamic> get popularProductList => _popularProductList;
+  List<ProductModel> _popularProductList = [];
+  List<ProductModel> get popularProductList => _popularProductList;
 
+  bool _isLoaded = false;
+  bool get isLoaded => isLoaded;
   Future<void> getPopularProductList() async {
     Response response = await popularProductRepo.getPopularProductList();
     if (response.statusCode == 200) {
       print("got products");
       _popularProductList = [];
       _popularProductList.addAll(Product.fromJson(response.body).products);
+      //print(_popularProductList);
+      _isLoaded = true;
       update();
     } else {}
   }
+  //c
 }
