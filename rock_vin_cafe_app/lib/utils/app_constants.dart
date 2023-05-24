6878288c06733cea87a878585b34f8817f3968da
Zaliye Prasandike <73892701+Saliya99@users.noreply.github.com>
2023-05-25 01:18:53 +0530

    Update constant

--- rock_vin_cafe_app/lib/utils/app_constants.dart
+++ rock_vin_cafe_app/lib/utils/app_constants.dart
@@ -5,6 +5,7 @@ class AppConstants {
 
   static const String POPULAR_PRODUCT_URI = "/api/v1/products/popular";
   static const String RECOMMENDED_PRODUCT_URI = "/api/v1/products/recommended";
+  static const String RECOMMENDED_PRODUCT_URI = "/api/v1/products/recommended";
 
   static const String TOKEN = "DBtoken";
 }
