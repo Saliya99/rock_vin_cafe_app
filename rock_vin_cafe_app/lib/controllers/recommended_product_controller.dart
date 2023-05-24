093c4b7318591d2af6f4f0ea870e0b7d0d60646f
Gagana Chiranjitha <gaganachiranjithacoc2@gmail.com>
2023-05-24 13:00:31 +0530

    recommwnded_product_list api

---
+++ rock_vin_cafe_app/lib/controllers/recommended_product_controller.dart
@@ -0,0 +1,26 @@
+import 'package:get/get.dart';
+import 'package:rock_vin_cafe_app/models/products_model.dart';
+import '../data/repository/recommended_product_repo.dart';
+
+class RecommendedProductController extends GetxController {
+  final RecommendedProductRepo recommendedProductRepo;
+  RecommendedProductController({required this.recommendedProductRepo});
+  List<ProductModel> _recommendedProductList = [];
+  List<ProductModel> get recommendedProductList => _recommendedProductList;
+
+  bool _isLoaded = false;
+  bool get isLoaded => isLoaded;
+  Future<void> getRecommendedProductList() async {
+    Response response =
+        await recommendedProductRepo.getRecommendedProductList();
+    if (response.statusCode == 200) {
+      print("got products");
+      _recommendedProductList = [];
+      _recommendedProductList.addAll(Product.fromJson(response.body).products);
+      //print(_popularProductList);
+      _isLoaded = true;
+      update();
+    } else {}
+  }
+  //c
+}
