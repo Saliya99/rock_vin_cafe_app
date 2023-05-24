6b0b89c56f14e3273bb52510f1d2370b1123f80d
Zaliye Prasandike <73892701+Saliya99@users.noreply.github.com>
2023-05-25 02:41:37 +0530

    remove ! from model

--- rock_vin_cafe_app/lib/models/products_model.dart
+++ rock_vin_cafe_app/lib/models/products_model.dart
@@ -23,7 +23,7 @@ class Product {
     if (json['products'] != null) {
       _products = <ProductModel>[];
       json['products'].forEach((v) {
-        _products!.add(ProductModel.fromJson(v));
+        _products.add(ProductModel.fromJson(v));
       });
     }
   }
