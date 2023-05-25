52f6f22083f60b6bcfb9c6298ad061cdb17c6146
Zaliye Prasandike <73892701+Saliya99@users.noreply.github.com>
2023-05-25 23:49:55 +0530

    dependencies added

--- rock_vin_cafe_app/lib/helper/dependencies.dart
+++ rock_vin_cafe_app/lib/helper/dependencies.dart
@@ -1,6 +1,10 @@
 import 'package:get/get.dart';
+import 'package:rock_vin_cafe_app/controllers/cart_controller.dart';
 import 'package:rock_vin_cafe_app/controllers/popular_product_controller.dart';
+import 'package:rock_vin_cafe_app/controllers/recommended_product_controller.dart';
+import 'package:rock_vin_cafe_app/data/repository/cart_repo.dart';
 import 'package:rock_vin_cafe_app/data/repository/popular_product_repo.dart';
+import 'package:rock_vin_cafe_app/data/repository/recommended_product_repo.dart';
 import 'package:rock_vin_cafe_app/utils/app_constants.dart';
 
 import '../data/api/api_client.dart';
@@ -11,7 +15,12 @@ Future<void> init() async {
 
   //repos
   Get.lazyPut(() => PopularProductRepo(apiClient: Get.find()));
+  Get.lazyPut(() => RecommendedProductRepo(apiClient: Get.find()));
+  Get.lazyPut(() => CartRepo());
+
 
   //controller
   Get.lazyPut(() => PopularProductController(popularProductRepo: Get.find()));
+  Get.lazyPut(() => RecommendedProductController(recommendedProductRepo: Get.find()));
+  Get.lazyPut(() => CartController(cartRepo: Get.find()));
 }
