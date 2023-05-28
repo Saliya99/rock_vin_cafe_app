b2a8ecac23b4727b5f35c8199f9a4174d1828fa6
Zaliye Prasandike <73892701+Saliya99@users.noreply.github.com>
2023-05-29 01:06:34 +0530

    Update Cart

--- rock_vin_cafe_app/lib/pages/cart/cart_page.dart
+++ rock_vin_cafe_app/lib/pages/cart/cart_page.dart
@@ -1,12 +1,15 @@
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'package:rock_vin_cafe_app/controllers/cart_controller.dart';
+import 'package:rock_vin_cafe_app/controllers/popular_product_controller.dart';
 import 'package:rock_vin_cafe_app/pages/home/main_food_page.dart';
 import 'package:rock_vin_cafe_app/utils/app_constants.dart';
 import 'package:rock_vin_cafe_app/utils/dimensions.dart';
 import 'package:rock_vin_cafe_app/widgets/app_icon.dart';
 import 'package:rock_vin_cafe_app/widgets/small_text.dart';
 
+import '../../controllers/recommended_product_controller.dart';
+import '../../routes/route_helper.dart';
 import '../../utils/colors.dart';
 import '../../widgets/big_text.dart';
 
@@ -33,7 +36,7 @@ top: Dimensions.height20*3,
                  SizedBox(width:Dimensions.width20*5 ,),
                  GestureDetector(
                    onTap: (){
-                    Get.to(()=>MainFoodPage());
+                     Get.toNamed(RouteHelper.getInitial());
                    },
                    child: AppIcon(icon: Icons.home_outlined,
                      iconColor: Colors.white,
@@ -62,8 +65,9 @@ top: Dimensions.height20*3,
                 context: context,
                 removeTop: true,
                 child: GetBuilder<CartController>(builder: (cartController){
+                  var _cartList=cartController.getItems;
                   return ListView.builder(
-                      itemCount: cartController.getItems.length,
+                      itemCount: _cartList.length,
                       itemBuilder:  (_,index){
                         return Container(
                           width:double.maxFinite,
@@ -71,19 +75,39 @@ top: Dimensions.height20*3,
 
                           child: Row(
                             children: [
-                              Container(
-                                width: Dimensions.height20*5,
-                                height: Dimensions.height20*5,
-                                margin: EdgeInsets.only(bottom: Dimensions.height10),
-                                decoration: BoxDecoration(
-                                  image: DecorationImage(
-                                      fit: BoxFit.cover,
-                                      image: NetworkImage(
-                                        'https://img.restaurantguru.com/r7a5-Cafes-Reck-bar-counter.jpg'
-                                        //AppConstants.BASE_URL+AppConstants.UPLOAD_URL+cartController.getItems[index].img!
-                                      )
+                              GestureDetector(
+                                onTap: (){
+                               var propularIndex=Get.find<PopularProductController>()
+                                   .popularProductList
+                                    .indexOf(_cartList[index].product!);
+                               if(propularIndex>=0){
+                                 Get.toNamed(RouteHelper.getPopularFood(propularIndex,"cartpage"));
+                                  
+                                 
+                               }else{
+                                 var recommendedIndex=Get.find<RecommendedProductController>()
+                                     .recommendedProductList
+                                     .indexOf(_cartList[index].product!);
+
+                                 Get.toNamed(RouteHelper.getRecommendedFood(recommendedIndex,"cartpage"));
+
+                               }
+
+                        },
+                                child: Container(
+                                  width: Dimensions.height20*5,
+                                  height: Dimensions.height20*5,
+                                  margin: EdgeInsets.only(bottom: Dimensions.height10),
+                                  decoration: BoxDecoration(
+                                    image: DecorationImage(
+                                        fit: BoxFit.cover,
+                                        image: NetworkImage(
+                                          'https://img.restaurantguru.com/r7a5-Cafes-Reck-bar-counter.jpg'
+                                          //AppConstants.BASE_URL+AppConstants.UPLOAD_URL+cartController.getItems[index].img!
+                                        )
+                                    ),
+                                    borderRadius: BorderRadius.circular(Dimensions.radius20),
                                   ),
-                                  borderRadius: BorderRadius.circular(Dimensions.radius20),
                                 ),
                               ),
                               SizedBox(width: Dimensions.widtht10,),
@@ -113,17 +137,17 @@ top: Dimensions.height20*3,
                                             children: [
                                               GestureDetector(
                                                   onTap: () {
-                                                    // popularProduct.setQuantity(false);
+                                                    cartController.addItem(_cartList[index].product!, -1);
                                                   },
                                                   child:
                                                   Icon(Icons.remove, color: AppColors.signColor)),
                                               SizedBox(
                                                 width: Dimensions.widtht10 / 2,
                                               ),
-                                              BigText(text:"0"),// popularProduct.inCartItems.toString()),
+                                              BigText(text:_cartList[index].quantity.toString()),// popularProduct.inCartItems.toString()),
                                               GestureDetector(
                                                   onTap: () {
-                                                    //.setQuantity(true);
+                                                    cartController.addItem(_cartList[index].product!, 1);
                                                   },
                                                   child: Icon(Icons.add, color: AppColors.signColor)),
                                               SizedBox(
@@ -148,6 +172,72 @@ top: Dimensions.height20*3,
         ))
       ],
     ),
+        bottomNavigationBar: GetBuilder<CartController>(
+          builder: (cartController) {
+            return Container(
+              height: Dimensions.bottomHeightBar,
+              padding: EdgeInsets.only(
+                  top: Dimensions.height30,
+                  bottom: Dimensions.height30,
+                  left: Dimensions.width20,
+                  right: Dimensions.width20),
+              decoration: BoxDecoration(
+                  color: AppColors.buttonBackgroungColor,
+                  borderRadius: BorderRadius.only(
+                      topLeft: Radius.circular(Dimensions.radius20 * 2),
+                      topRight: Radius.circular(Dimensions.radius20 * 2))),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  Container(
+                    padding: EdgeInsets.only(
+                        top: Dimensions.height20,
+                        bottom: Dimensions.height20,
+                        left: Dimensions.width20,
+                        right: Dimensions.width20),
+                    decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(Dimensions.radius20),
+                      color: Colors.white,
+                    ),
+                    child: Row(
+                      children: [
+
+                        SizedBox(
+                          width: Dimensions.widtht10 / 2,
+                        ),
+                        BigText(text:"Rs."+cartController.totalAmount.toString()),
+
+                        SizedBox(
+                          width: Dimensions.widtht10 / 2,
+                        ),
+                      ],
+                    ),
+                  ),
+                  GestureDetector(
+                    onTap: () {
+                     // popularProduct.addItem(product);
+                    },
+                    child: Container(
+                      padding: EdgeInsets.only(
+                          top: Dimensions.height20,
+                          bottom: Dimensions.height20,
+                          left: Dimensions.width20,
+                          right: Dimensions.width20),
+                      decoration: BoxDecoration(
+                          borderRadius:
+                          BorderRadius.circular(Dimensions.radius20),
+                          color: AppColors.mainColor),
+                      child: BigText(
+                        text: " Check out",
+                        color: Colors.white,
+                      ),
+                    ),
+                  )
+                ],
+              ),
+            );
+          },
+        )
     );
   }
 }
