f3f8ce5b907b9f3d9b3901b63014b81b552c1ca2
Zaliye Prasandike <73892701+Saliya99@users.noreply.github.com>
2023-05-28 23:33:32 +0530

    update

--- rock_vin_cafe_app/lib/pages/cart/cart_page.dart
+++ rock_vin_cafe_app/lib/pages/cart/cart_page.dart
@@ -9,7 +9,6 @@ import 'package:rock_vin_cafe_app/widgets/small_text.dart';
 
 import '../../utils/colors.dart';
 import '../../widgets/big_text.dart';
-import 'package:get/get.dart ';
 
 class CartPage extends StatelessWidget {
   const CartPage({Key? key}) : super(key: key);
