f408bb180dacf0ef94fe424f32e16e18a2337728
Zaliye Prasandike <73892701+Saliya99@users.noreply.github.com>
2023-06-07 06:57:51 +0530

    Update LandinPage

--- rock_vin_cafe_app/lib/landing _page.dart
+++ rock_vin_cafe_app/lib/landing _page.dart
@@ -3,8 +3,13 @@ import 'package:flutter/material.dart';
 import 'package:flutter/src/widgets/framework.dart';
 import 'package:flutter/src/widgets/placeholder.dart';
 import 'package:provider/provider.dart';
-import 'package:rock_vin_cafe_app/pages/auth/login.dart';
+import 'package:rock_vin_cafe_app/controllers/database_controller.dart';
+import 'package:rock_vin_cafe_app/pages/auth/login_email.dart';
+import 'package:rock_vin_cafe_app/pages/auth/login_phone.dart';
 import 'package:rock_vin_cafe_app/pages/auth/welocme.dart';
+import 'package:rock_vin_cafe_app/pages/home/home.dart';
+import 'package:rock_vin_cafe_app/pages/home/home_page_2.dart';
+import 'package:rock_vin_cafe_app/pages/profile/profile.dart';
 import 'package:rock_vin_cafe_app/routes/route_helper.dart';
 
 import 'controllers/auth_controller.dart';
@@ -22,12 +27,15 @@ class LandingPage extends StatelessWidget {
         if (snapshot.connectionState == ConnectionState.active) {
           final User? user = snapshot.data;
           if (user == null) {
-            //return SignInPage();
+            // return LoginByPhonePage();
             return WelcomePage();
           }
           //return HomePage();
-          auth.signOut();
-          return const Placeholder();
+          // auth.signOut();
+          return Provider<Database>(
+            create: (context) => SQLDatabase(),
+            child: HomePage(),
+          );
         }
         return const Scaffold(
           body: Center(
