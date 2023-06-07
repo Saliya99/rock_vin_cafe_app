96f6f022aa4103ecf97a40967834235a88bdaabe
Zaliye Prasandike <73892701+Saliya99@users.noreply.github.com>
2023-06-07 06:59:22 +0530

    Update route_helper

--- rock_vin_cafe_app/lib/routes/route_helper.dart
+++ rock_vin_cafe_app/lib/routes/route_helper.dart
@@ -1,11 +1,12 @@
 import 'package:get/get.dart';
 import 'package:rock_vin_cafe_app/landing%20_page.dart';
+import 'package:rock_vin_cafe_app/pages/auth/login_phone.dart';
 import 'package:rock_vin_cafe_app/pages/cart/cart_page.dart';
 import 'package:rock_vin_cafe_app/pages/food/popular_food_detail.dart';
 import 'package:rock_vin_cafe_app/pages/food/recommended_food_detail.dart';
 import 'package:rock_vin_cafe_app/pages/home/main_food_page.dart';
 
-import '../pages/auth/login.dart';
+import '../pages/auth/login_email.dart';
 
 class RouteHelper {
   static const String initial = "/";
@@ -13,7 +14,8 @@ class RouteHelper {
   static const String recommendedFood = "/recommended-food";
   static const String cartPage = "/cart-page";
   static const String landingPage = "/landing-page";
-  static const String loginPage = "/login-page";
+  static const String loginbyEmailPage = "/login-by-email-page";
+  static const String loginbyPhonePage = "/login-by-phone-page";
 
   static String getInitial() => '$initial';
   static String getPopularFood(int pageId, String page) =>
@@ -26,10 +28,13 @@ class RouteHelper {
 
   static String getLandingPage() => '$landingPage';
 
-  static String getLoginPage() => '$loginPage';
+  static String getLoginPage() => '$loginbyEmailPage';
+
+  static String getLoginByPhonePage() => '$loginbyPhonePage';
 
   static List<GetPage> routes = [
-    GetPage(name: loginPage, page: () => LoginPage()),
+    GetPage(name: loginbyEmailPage, page: () => LoginByEmailPage()),
+    GetPage(name: loginbyPhonePage, page: () => LoginByPhonePage()),
     GetPage(name: landingPage, page: () => LandingPage()),
     GetPage(name: initial, page: () => MainFoodPage()),
     GetPage(
