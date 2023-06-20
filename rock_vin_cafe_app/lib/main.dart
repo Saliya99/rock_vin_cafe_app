91c7645de9b9b10a994ba8657036280ea0db5fed
Gagana Chiranjitha <103405715+gaganachiranjitha99@users.noreply.github.com>
2023-06-20 23:57:44 +0530

    main.dart update

--- rock_vin_cafe_app/lib/main.dart
+++ rock_vin_cafe_app/lib/main.dart
@@ -1,20 +1,17 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 
 import 'package:get/get.dart';
-import 'package:rock_vin_cafe_app/controllers/recommended_product_controller.dart';
-import 'package:rock_vin_cafe_app/pages/cart/cart_page.dart';
-import 'package:rock_vin_cafe_app/pages/food/popular_food_detail.dart';
-import 'package:rock_vin_cafe_app/pages/food/recommended_food_detail.dart';
-import 'package:rock_vin_cafe_app/pages/home/home_page.dart';
-import 'package:rock_vin_cafe_app/pages/home/main_food_page.dart';
+import 'package:provider/provider.dart';
+import 'package:responsive_sizer/responsive_sizer.dart';
+import 'package:rock_vin_cafe_app/controllers/auth_controller.dart';
+import 'package:rock_vin_cafe_app/controllers/database_controller.dart';
 import 'package:rock_vin_cafe_app/routes/route_helper.dart';
-import 'controllers/popular_product_controller.dart';
-import 'package:rock_vin_cafe_app/pages/home/food_page_body.dart';
-import 'helper/dependencies.dart' as dep;
 
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
-  await dep.init();
+  await Firebase.initializeApp();
+
   runApp(const MyApp());
 }
 
@@ -24,14 +21,24 @@ class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    Get.find<PopularProductController>().getPopularProductList();
-    Get.find<RecommendedProductController>().getRecommendedProductList();
-    return GetMaterialApp(
-      debugShowCheckedModeBanner: false,
-      title: 'RockVin Cafe',
-      home: HomePage(),
-      initialRoute: RouteHelper.getInitial(),
-      getPages: RouteHelper.routes,
-    );
+    return ResponsiveSizer(builder: (context, orientation, screenType) {
+      return MultiProvider(
+        providers: [
+          Provider<AuthBase>(
+            create: (context) => Auth(),
+          ),
+          Provider<Database>(
+            create: (context) => SQLDatabase(),
+          ),
+        ],
+        child: GetMaterialApp(
+          debugShowCheckedModeBanner: false,
+          title: 'RockVin Cafe',
+          //home: MainFoodPage(),
+          initialRoute: RouteHelper.getLandingPage(),
+          getPages: RouteHelper.routes,
+        ),
+      );
+    });
   }
 }
