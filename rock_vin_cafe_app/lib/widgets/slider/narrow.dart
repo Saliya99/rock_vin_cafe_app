f3c3ee462e14f0dc913df7407d292e021a3da29b
Gagana Chiranjitha <103405715+gaganachiranjitha99@users.noreply.github.com>
2023-06-21 14:13:50 +0530

    updated

--- rock_vin_cafe_app/lib/widgets/slider/narrow.dart
+++ rock_vin_cafe_app/lib/widgets/slider/narrow.dart
@@ -7,5 +7,20 @@ class ArrawNavsCustom extends StatelessWidget {
   final Function goNextPage;
   final Function goPrevPage;
 
+  @override
+  Widget build(BuildContext context) {
+    return Row(mainAxisAlignment: MainAxisAlignment.center, children: [
+      IconButton(
+        onPressed: () => goPrevPage(),
+        icon: const Icon(Icons.arrow_back_ios_rounded),
+      ),
+      const SizedBox(
+        width: 16,
+      ),
+      IconButton(
+        onPressed: () => goNextPage(),
+        icon: const Icon(Icons.arrow_forward_ios_rounded),
+      ),
+    ]);
   }
 }
