fbfc52cc55f34778c15e031faa19e51d69d1d1a1
Gagana Chiranjitha <103405715+gaganachiranjitha99@users.noreply.github.com>
2023-06-21 14:02:50 +0530

    narrow.dart

---
+++ rock_vin_cafe_app/lib/widgets/slider/narrow.dart
@@ -0,0 +1,26 @@
+import 'package:flutter/material.dart';
+
+class ArrawNavsCustom extends StatelessWidget {
+  const ArrawNavsCustom(
+      {super.key, required this.goNextPage, required this.goPrevPage});
+
+  final Function goNextPage;
+  final Function goPrevPage;
+
+  @override
+  Widget build(BuildContext context) {
+    return Row(mainAxisAlignment: MainAxisAlignment.center, children: [
+      IconButton(
+        onPressed: () => goPrevPage(),
+        icon: const Icon(Icons.arrow_back_ios_rounded),
+      ),
+      const SizedBox(
+        width: 16,
+      ),
+      IconButton(
+        onPressed: () => goNextPage(),
+        icon: const Icon(Icons.arrow_forward_ios_rounded),
+      ),
+    ]);
+  }
+}
