b71c3b8aad898972cd7deccd70156c929f0cfd40
Gagana Chiranjitha <103405715+gaganachiranjitha99@users.noreply.github.com>
2023-06-22 08:06:35 +0530

    cardcupdated

--- rock_vin_cafe_app/lib/models/card_columns.dart
+++ rock_vin_cafe_app/lib/models/card_columns.dart
@@ -14,16 +14,26 @@ class CardModel {
     required this.exp,
     required this.username,
   });
+  factory CardModel.fromMap(Map<String, dynamic> map) {
+    return CardModel(
+      cardId: int.parse(map['card_id'].toString()),
+      nameOnCard: map['name_on_card'] as String,
+      cardNo: map['cardno'] as String,
+      csvNo: int.parse(map['csvno'] as String),
+      exp: map['exp'] as String,
+      username: map['username'] as String,
+    );
+  }
 
-  List<dynamic> dataToList() {
-    return [
-      cardId,
-      nameOnCard,
-      cardNo,
-      csvNo,
-      exp,
-      username,
-    ];
+  Map<String, dynamic> toMap() {
+    return {
+      'card_id': cardId,
+      'name_on_card': nameOnCard,
+      'cardno': cardNo,
+      'csvno': csvNo,
+      'exp': exp,
+      'username': username,
+    };
   }
 
   String tableColumns() {
